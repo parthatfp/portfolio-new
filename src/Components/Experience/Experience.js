b4d48f2b580fc295fa22b11325aa4851c6349850
Partha Proteem Ghose <96822921+parthatfp@users.noreply.github.com>
2022-08-27 18:55:45 +0600

    experience done

---
+++ src/Components/Experience/Experience.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import './Experience.css';
+
+export default function Experience() {
+  return (
+    <div className="experience">
+        <div className="achievement">
+            <div className="circle">8+</div>
+            <span>years</span>
+            <span>Experience</span>
+        </div>
+        <div className="achievement">
+            <div className="circle">20+</div>
+            <span>complete</span>
+            <span>Projects</span>
+        </div>
+        <div className="achievement">
+            <div className="circle">5+</div>
+            <span>companies</span>
+            <span>Work</span>
+        </div>
+    </div>
+  )
+}
